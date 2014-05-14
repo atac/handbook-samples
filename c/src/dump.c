bb659c4fc04bd041ffdca151ba742a0be874cf1e
Micah Ferrill <mcferrill@gmail.com>
2014-05-14 13:24:13 -0500

    Started dump project.

---
+++ c/src/dump.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "irig106ch10.h"
+#include "copy_args.c"
+#include "common.h"
+
+int main(int argc, char ** argv){
+	printf("Hello world!");
+
+	return quit(0);
+}

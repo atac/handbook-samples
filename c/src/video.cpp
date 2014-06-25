f674419dcbde0576c5cc26a348d27e3f22496d80
Micah Ferrill <mcferrill@gmail.com>
2014-06-25 15:32:38 -0500

    Video loading working\!

--- c/src/video.cpp
+++ c/src/video.cpp
@@ -2,6 +2,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <direct.h>
 #include <Windows.h>
 
 #include <gl\GLU.h>
@@ -21,6 +23,17 @@
 #include "qmpwidget.cpp"
 
 
+// Byteswap a buffer.
+void swap(char *p, int len) {
+	char tmp;
+	for (int i = 0; i < ((len / 2)); i++) {
+		tmp = p[i * 2];
+		p[i * 2] = p[(i * 2) + 1];
+		p[(i * 2) + 1] = tmp;
+	}
+}
+
+
 // The loader thread //
 
 Loader::Loader(QWidget * parent, QString filename){
@@ -28,18 +41,78 @@ Loader::Loader(QWidget * parent, QString filename){
 }
 
 void Loader::run(){
-	FILE * out[0x10000];
-	for (int i = 0; i < 0x10000; i++){
+
+	// Create output containers
+	FILE * out[0x1000];
+	for (int i = 0; i < 0x1000; i++){
 		out[i] = NULL;
 	}
 
+	// Open input file.
 	int input_handle;
-	const char* path = this->filename.toUtf8();
-	Irig106::EnI106Status status = Irig106::enI106Ch10Open(&input_handle, path, Irig106::I106_READ);
+	Irig106::EnI106Status status = Irig106::enI106Ch10Open(&input_handle, this->filename.toUtf8().constData(), Irig106::I106_READ);
 	if (status != Irig106::I106_OK){
 		qCritical() << "Error opening source file: ";
 		return;
 	}
+
+	// Parse loop.
+	Irig106::SuI106Ch10Header header;
+	void * buffer = malloc(24);
+	void * ts = malloc(188);
+	while (1){
+
+		// Read next header or exit.
+		status = Irig106::enI106Ch10ReadNextHeader(input_handle, &header);
+		if (status != Irig106::I106_OK){
+			qDebug() << "Finished";
+			return;
+		}
+
+		// Ensure that we're interested in this particular packet.
+		if (0x3F > header.ubyDataType || header.ubyDataType > 0x43){
+			continue;
+		}
+
+		// Create a temporary file for this channel.
+		_mkdir("tmp");
+
+		char path[1000];
+		strcpy(path, "./tmp");
+		char channel[5];
+		sprintf(channel, "/%d", header.uChID);
+		strcat(path, channel);
+		strcat(path, ".mpg");
+
+		if (out[header.uChID] == NULL){
+			qDebug() << "Opening file: ";
+			qDebug() << path;
+			out[header.uChID] = fopen(path, "wb");
+
+			if (out[header.uChID] == NULL){
+				qCritical() << ("Error opening output file: %s", path);
+				continue;
+			}
+		}
+
+		// Read packet data.
+		buffer = realloc(buffer, header.ulPacketLen);
+		status = Irig106::enI106Ch10ReadDataFile(input_handle, header.ulPacketLen, buffer);
+		if (status != Irig106::I106_OK){
+			qCritical() << "Error reading packet.";
+			continue;
+		}
+
+		// Ignore first 4 bytes (CSDW)
+		BYTE * data = (BYTE *)buffer + 4;
+		int datalen = header.ulDataLen - 4;
+		for (int i = 0; i < (datalen / 188); i++){
+			memcpy(ts, data + (i * 188), 188);
+			swap((char *)ts, 188);
+			fwrite(ts, 1, 188, out[header.uChID]);
+		}
+	}
+
 }
 
 
@@ -65,7 +138,7 @@ video::video(QWidget *parent)
 	//vid->setVideoOutput(QString("directx:noaccel"));
 	QString winid = QString::number((int)(container->winId()));
 	vid->start(QStringList("-wid") << winid);
-	vid->load("14.mpg");
+	//vid->load("14.mpg");
 	grid->addWidget(container, 0, 0);
 
 	player = vid->process();

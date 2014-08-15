8748a38f38e6ea82e3a83be6a6498f92c4320d8b
Micah Ferrill <mcferrill@gmail.com>
2014-08-15 16:31:08 -0500

    More work on C reindex sample.

--- c/src/reindex.c
+++ c/src/reindex.c
@@ -8,12 +8,67 @@
 #include "reindex_args.c"
 #include "common.h"
 
+void gen_node(int64_t offset, SuI106Ch10Header * packets, int packets_c, int64_t offsets[], FILE * output, int seq){
+	printf("Index node for %d packets\n", packets_c);
+
+	// Packet header
+	SuI106Ch10Header index_header;
+	index_header.uSync = 0xeb25;
+	index_header.uChID = 0;
+	index_header.ulPacketLen = 36 + (20 * packets_c);
+	index_header.ulDataLen = 12 + (20 * packets_c);
+	index_header.ubyHdrVer = 0x06;
+	index_header.ubySeqNum = seq;
+	index_header.ubyPacketFlags = 0;
+	index_header.ubyDataType = 0x03;
+	index_header.uChecksum = 0;
+	uint64_t sums = 0xeb25 + (12 + (20 * packets_c)) * 2;
+	sums += 24 + 6 + seq + 3;
+	for (int i = 0; i <= 6; i++){
+		sums += index_header.aubyRefTime[i];
+	}
+	sums &= 0xffff;
+	index_header.uChecksum = (uint16_t)sums;
+	fwrite(&index_header, 24, 1, output);
+
+	// CSDW
+	int32_t csdw = 0;
+	csdw &= (1 << 31);
+	csdw &= (1 << 30);
+	fwrite(&csdw, 4, 1, output);
+
+	// File size
+	fwrite(&offset, 8, 1, output);
+
+	// Packets
+	for (int i = 0; i <= packets_c; i++){
+		SuI106Ch10Header packet = packets[i];
+		
+		// IPTS
+		fwrite(index_header.aubyRefTime, 1, 6, output);
+
+		// Index entry
+		int8_t filler = 0;
+		fwrite(&filler, 1, 1, output);
+		fwrite(&packet.ubyDataType, 1, 1, output);
+		fwrite(&packet.uChID, 2, 1, output);
+		fwrite(&offsets[i], 8, 1, output);
+	}
+}
+
+int increment(seq){
+	seq++;
+	if (seq > 0xFF){
+		seq = 0;
+	}
+	return seq;
+}
+
 int main(int argc, char ** argv){
 	DocoptArgs args = docopt(argc, argv, 1, "1");
 	int input_handle;
 	SuI106Ch10Header header;
-	FILE *output;
-	int packets = 0;
+	FILE * output;
 	void * buffer = malloc(24);
 
 	// Validate arguments and offer help.
@@ -31,30 +86,23 @@ int main(int argc, char ** argv){
 	}
 
 	// Open output file.
-	output = fopen(argv[2], "w");
+	output = fopen(argv[2], "wb");
 	if (output == NULL){
 		return error("Couldn't open destination file.");
 	}
 
-	// Copy TMATS
-	status = enI106Ch10ReadNextHeader(input_handle, &header);
-	if (status != I106_OK){
-		printf("Finished");
-		return quit(0);
-	}
-	buffer = realloc(buffer, header.ulPacketLen);
-	status = enI106Ch10ReadDataFile(input_handle, header.ulPacketLen, buffer);
-	if (status != I106_OK){
-		printf("Error reading TMATS.");
-		return quit(0);
-	}
-
-	// Write TMATS out.
-	fwrite(&header, sizeof(header), 1, output);
-	fwrite(&buffer, header.ulPacketLen, 1, output);
+	int packets_c = 0;
+	SuI106Ch10Header packets_arr[20000];
+	int64_t offsets[20000];
+	SuI106Ch10Header last_packet;
+	int last_packet_at;
+	int node_seq = 0;
+	int64_t offset;
 
 	// Parse loop.
 	while (1){
+
+		enI106Ch10GetPos(input_handle, &offset);
 
 		// Read next header or exit.
 		status = enI106Ch10ReadNextHeader(input_handle, &header);
@@ -76,13 +124,32 @@ int main(int argc, char ** argv){
 			continue;
 		}
 
-		// Write packet to file.
-		fwrite(&header, sizeof(header), 1, output);
-		fwrite(&buffer, header.ulPacketLen, 1, output);
+		if (header.ubyDataType != 0x03){
+			// Write packet to file.
+			fwrite(&header, sizeof(header), 1, output);
+			fwrite(&buffer, header.ulDataLen, 1, output);
+
+			last_packet = header;
+			//enI106Ch10GetPos(input_handle, &last_packet_at);
+			packets_c++;
+			packets_arr[packets_c] = header;
+			offsets[packets_c] = offset - header.ulPacketLen;
+		}
+
+		if (args.strip){
+			continue;
+		}
+
+		if ((header.ubyDataType == 0x02 || header.ubyDataType == 0x11) || (packets_c > 20000)){
+			// Generate node packet.
+			gen_node(offset, &packets_arr, packets_c, offsets, output, node_seq);
+			node_seq = increment(node_seq);
+			packets_c = 0;
+		}
+
 	}
 
 	if (args.strip){
 		printf("Stripped existing indices.");
 	}
-
 }

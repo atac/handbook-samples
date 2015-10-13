e8f06928f496cb1e19dfac68c0c91079f7a491c4
Micah Ferrill <mcferrill@gmail.com>
2015-10-13 10:31:49 -0500

    Added copyright notices to basic samples.

--- pyc10/stat.py
+++ pyc10/stat.py
@@ -1,5 +1,41 @@
 #!/usr/bin/env python
 
+"""
+  stat.py - Display some basic information about the channels within a
+    Chapter 10 file.
+
+ Copyright (c) 2015 Micah Ferrill
+
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are
+ met:
+
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+
+   * Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+
+   * Neither the name Irig106.org nor the names of its contributors may
+     be used to endorse or promote products derived from this software
+     without specific prior written permission.
+
+ This software is provided by the copyright holders and contributors
+ "as is" and any express or implied warranties, including, but not
+ limited to, the implied warranties of merchantability and fitness for
+ a particular purpose are disclaimed. In no event shall the copyright
+ owner or contributors be liable for any direct, indirect, incidental,
+ special, exemplary, or consequential damages (including, but not
+ limited to, procurement of substitute goods or services; loss of use,
+ data, or profits; or business interruption) however caused and on any
+ theory of liability, whether in contract, strict liability, or tort
+ (including negligence or otherwise) arising in any way out of the use
+ of this software, even if advised of the possibility of such damage.
+"""
+
 __doc__ = """usage: stat.py <file> [options]
 
 Options:
@@ -45,8 +81,8 @@ if __name__ == '__main__':
         if channel_index is None:
             channel_index = len(channels)
             channels.append({'packets': 0,
-                            'type': packet.data_type,
-                            'id': packet.channel_id})
+                             'type': packet.data_type,
+                             'id': packet.channel_id})
 
         # Increment the counter.
         channels[channel_index]['packets'] += 1

44c58b126b45174c55cf5a15c4054d81c1308417
Micah Ferrill <mcferrill@gmail.com>
2019-12-20 13:09:01 -0600

    Updating based on c10-tools and i106 libraries.

--- python/common.py
+++ python/common.py
@@ -1,7 +1,86 @@
 
+from __future__ import print_function
+from datetime import timedelta
 import os
 
 from tqdm import tqdm
+
+
+# Format a number nicely with commas for thousands, etc.
+fmt_number = '{0:,}'.format
+
+
+def find_c10(paths):
+    """Take a list of paths and yield a Chapter 10 files found at
+    those locations or subdirectories.
+    """
+
+    for path in paths:
+        path = os.path.abspath(path)
+        if os.path.isdir(path):
+            for dirname, dirnames, filenames in os.walk(path):
+                for f in filenames:
+                    if os.path.splitext(f)[1].lower() in ('.c10', '.ch10'):
+                        yield os.path.join(dirname, f)
+        else:
+            yield path
+
+
+def print_table(table):
+    """Print tabular data to stdout. Numeric fields justified right, others
+    left.
+    """
+
+    col_width = [max(len(x) for x in col) for col in zip(*table)]
+
+    # Header row
+    line = '-' * (sum(col_width) + (2 * (len(table[0]) + 2)))
+    print(line)
+    print('|', end=' ')
+    for i, x in enumerate(table[0]):
+        print((x.rjust(col_width[i]) if x.isdigit()
+               else x.ljust(col_width[i])) + ' |', end=' ')
+    print()
+    print(line)
+
+    # Rows
+    for row in table[1:]:
+        print('|', end=' ')
+        for i, x in enumerate(row):
+            print((x.rjust(col_width[i]) if x.isdigit()
+                   else x.ljust(col_width[i])) + ' |', end=' ')
+        print()
+
+    print(line)
+
+
+def get_time(rtc, time_packet):
+    """Get a datetime object based on last time packet and an RTC value."""
+
+    # pychapter10
+    if hasattr(time_packet, 'body'):
+        time_packet.body.parse()
+        t = time_packet.body.time
+
+    # libirig106-python
+    else:
+        t = time_packet.time
+
+    offset = (rtc - time_packet.rtc) / 10000000.0
+    t += timedelta(seconds=offset)
+    return t
+
+
+def fmt_size(size):
+    """Convert byte size to a more readable format (mb, etc.)."""
+
+    units = ['gb', 'mb', 'kb']
+    unit = 'b'
+    while size > 1024 and units:
+        size /= 1024.0
+        unit = units.pop()
+
+    return '%s %s' % (round(size, 2), unit)
 
 
 def walk_packets(c10, args={}):
--- python/stat.py
+++ python/stat.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 
-"""usage: stat.py <file> [options]
+"""usage: c10-stat <file> [options]
 
 Options:
-    -c CHANNEL..., --channel CHANNEL...  Specify channels to include(csv).
+    -c CHANNEL..., --channel CHANNEL...  Specify channels to include (csv).
     -e CHANNEL..., --exclude CHANNEL...  Specify channels to ignore (csv).
     -t TYPE, --type TYPE                 The types of data to show (csv, may \
-be decimal or hex eg: 0x40)."""
+be decimal or hex eg: 0x40).
+"""
+
+from __future__ import print_function
 
 from docopt import docopt
 
-from chapter10 import C10
+try:
+    from i106 import C10
+except ImportError:
+    from chapter10 import C10
 
-from walk import walk_packets
+from common import walk_packets, fmt_number, fmt_size, FileProgress
 
 
 TYPES = (
@@ -35,41 +41,45 @@ TYPES = (
 )
 
 
-if __name__ == '__main__':
+def main():
 
     # Get commandline args.
     args = docopt(__doc__)
 
     channels = {}
 
-    # Iterate over selected packets based on args.
-    for packet in walk_packets(C10(args['<file>'], True), args):
-        key = (packet.channel_id, packet.data_type)
-        if key not in channels:
-            channels[key] = {'packets': 0,
-                             'size': 0,
-                             'type': packet.data_type,
-                             'id': packet.channel_id}
+    with FileProgress(args['<file>']) as progress:
 
-        channels[key]['packets'] += 1
-        channels[key]['size'] += packet.packet_length
+        # Iterate over selected packets based on args.
+        for packet in walk_packets(C10(args['<file>']), args):
+            key = (packet.channel_id, packet.data_type)
+            if key not in channels:
+                channels[key] = {'packets': 0,
+                                 'size': 0,
+                                 'type': packet.data_type,
+                                 'id': packet.channel_id}
+
+            channels[key]['packets'] += 1
+            channels[key]['size'] += packet.packet_length
+
+            progress.update(packet.packet_length)
 
     # Print details for each channel.
-    print('Channel ID     Data Type' + 'Packets'.rjust(39), 'Size'.rjust(16))
+    print('Channel ID {:>13} {:>38} {:>16}'.format(
+        'Data Type', 'Packets', 'Size'))
     print('-' * 80)
     packets, size = 0, 0
     for key, channel in sorted(channels.items()):
-        print(('Channel %s' % channel['id']).ljust(15), end='')
-
         datatype = int(channel['type'] / 8.0)
         type_label = TYPES[datatype]
         subtype = channel['type'] - (datatype * 8)
 
-        print(('%s - %s (format %s)' % (hex(channel['type']),
+        print('Channel {:<7}{:<35}'.format(channel['id'],
+        ('%s - %s (format %s)' % (hex(channel['type']),
                                         type_label,
                                         subtype)).ljust(35), end='')
-        print('{0:,}'.format(channel['packets']).rjust(13), end='')
-        print('{0:,}'.format(channel['size']).rjust(17))
+        print(fmt_number(channel['packets']).rjust(13), end='')
+        print(fmt_size(channel['size']).rjust(17))
         packets += channel['packets']
         size += channel['size']
 
@@ -78,5 +88,12 @@ if __name__ == '__main__':
     print('''Summary for %s:
     Channels: %s
     Packets: %s
-    Size: %s''' % (args['<file>'], len(channels), '{0:,}'.format(packets),
-                   '{0:,}'.format(size)))
+    Size: %s''' % (
+        args['<file>'], len(channels), fmt_number(packets), fmt_size(size)))
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
